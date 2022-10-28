0aa65a30b1be0684d5ec3b4058142560f67ecf29
sabbirsaad <sabbir.saad00@gmail.com>
2022-10-28 06:02:31 +0600

    project completed

---
+++ src/components/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyD20AZpIBlJsorZheo56WHhvMByJu6ZGQY",
+    authDomain: "repair-geek.firebaseapp.com",
+    projectId: "repair-geek",
+    storageBucket: "repair-geek.appspot.com",
+    messagingSenderId: "176974271444",
+    appId: "1:176974271444:web:2ae7d4c057d8d0adf3bc41"
+  };
+  export default firebaseConfig;
